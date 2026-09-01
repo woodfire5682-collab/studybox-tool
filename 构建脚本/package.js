24704e4f7d1532cda6ffa3a1b05fe5d9aa3f0910
woodfire5682-collab <woodfire5682@gmail.com>
2026-09-01 16:59:11 +0800

    add: 构建脚本/package.js

---
+++ 构建脚本/package.js
@@ -0,0 +1,6 @@
+﻿const AdmZip=require('adm-zip'); const fs=require('fs');
+const zip=new AdmZip('D:/deepseek/_apkbuild/base.apk');
+zip.addFile('classes.dex', fs.readFileSync('D:/deepseek/_apkbuild/dex/classes.dex'));
+zip.addFile('assets/index.html', fs.readFileSync('D:/deepseek/_apkbuild/app/assets/index.html'));
+zip.writeZip('D:/deepseek/_apkbuild/staged.apk');
+console.log('staged.apk OK', fs.statSync('D:/deepseek/_apkbuild/staged.apk').size);
